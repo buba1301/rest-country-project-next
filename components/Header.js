4d2d095bab933f1b091295e1b4a7ba10852b1c48
Boris Muzalevskii <bmuza57@gmail.com>
2022-11-19 20:25:32 +0300

    Add styles for header and

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Moon from '../public/moon-outline.svg';
+
+import s from '../styles/Header.module.scss';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className={s.container}>
+      <h1 className={s.header}>Where in the world?</h1>
+      <button className={s.button}>Dark Mode</button>
+    </div>
+  );
 }
